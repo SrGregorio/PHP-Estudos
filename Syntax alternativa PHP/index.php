841fca2440a86858e96e913976db9df666e171c1
Renato Gregório <renatogregorio@protonmail.com>
2021-10-27 15:32:54 -0300

    Syntax alternativa

---
+++ Syntax alternativa PHP/index.php
@@ -0,0 +1,26 @@
+<?php
+	$nome = 'Renato';
+
+	if($nome == 'Renato'):
+		echo 'verdade';
+	endif;
+	echo '<br>';
+
+	$contador = 0;
+	while($contador<10):
+		$contador ++;
+		echo 'Oi';
+		echo '<br>';
+	endwhile;
+
+	$array = array();
+	foreach ($array as $key => $value):
+
+	endforeach;
+
+	for($i=0;$i<5;$i++):
+		echo 'oi';
+		echo '<hr>';
+	endfor;	
+
+?>
