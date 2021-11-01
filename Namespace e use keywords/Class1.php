e7ba0b55244a0d646043bcb961645e56e69a7274
Renato Gregório <renatogregorio@protonmail.com>
2021-11-01 19:33:16 -0300

    Namespace e use keywords

---
+++ Namespace e use keywords/Class1.php
@@ -0,0 +1,16 @@
+<?php
+	
+	namespace Sessao1;
+	use \Sessao1\Class1;
+	
+	class Class1
+	{
+		
+		function __construct()
+		{
+			echo '27 anos';
+		}
+	}
+
+
+?>
---
+++ Namespace e use keywords/Class2.php
@@ -0,0 +1,17 @@
+<?php
+	
+	namespace Sessao2;
+	use \Sessao2\Class2;
+
+	class Class2 
+	{
+		
+		function __construct()
+		{
+			echo 'Renato';
+			
+		}
+	}
+
+
+?>
